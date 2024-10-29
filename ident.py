6d5a94b77cb7fe035624663fc993e887505f5ec1
feelingproudindianarmy <celestialh4@gmail.com>
2024-10-29 18:00:47 +0545

    indenty op

---
+++ ident.py
@@ -0,0 +1,21 @@
+#identy op
+a=[1,2,8,9,0]
+b=[3.2,10,5,6]
+c=a
+print(c)
+print(a is c)
+print(a is b)
+print(a is not c)
+print(a is not b)
+#membership op
+print(a in c)
+print(a in b)
+print(a not in c)
+print(a not in b)
+#bitwise op
+a=5
+b=6
+print(bin(a))
+print(bin(b))
+print(a&b)
+print(a|b)
